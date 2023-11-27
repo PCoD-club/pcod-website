4b5be38de7f71757c6b2b1b23dafaaf55e3326c4
Amber Kirschbaum <mkirsch42@gmail.com>
2023-11-27 10:35:30 -0700

    test

--- netlify/functions/discord-collect/index.ts
+++ netlify/functions/discord-collect/index.ts
@@ -11,23 +11,25 @@ const {
 
 export const handler: Handler = async (event, context) => {
     if (event.headers["x-api-key"] != API_KEY) {
-        console.log("Invalid API Key");
-        console.log(event);
         return { statusCode: StatusCodes.UNAUTHORIZED, body: "Invalid API Key" };
     }
 
     const doc = new GoogleSpreadsheet(config.docId);
 
+    console.log("a");
     await doc.useServiceAccountAuth({
         client_email: GOOGLE_SERVICE_ACCOUNT_EMAIL,
         private_key: GOOGLE_PRIVATE_KEY.replaceAll('\\n', '\n'),
     });
+    console.log("b");
     await doc.loadInfo();
+    console.log("c");
     const sheet = doc.sheetsById[config.sheetId];
 
     const payload = JSON.parse(event.body);
     const values = [payload.key, payload.value];
     const row = await sheet.addRow(values);
+    console.log("d");
 
     return {
         statusCode: StatusCodes.OK,
